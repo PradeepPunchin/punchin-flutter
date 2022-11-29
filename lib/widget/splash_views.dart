43acf77e7c43cf1293a98adb6408f4901cf3ee87
sangamsharma <sangam.sharma@oodles.io>
2022-11-29 15:31:14 +0530

    setup

--- lib/widget/splash_views.dart
+++ lib/widget/splash_views.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -12,23 +13,22 @@ class _SplashScreenState extends State<SplashScreen> {
   //final UserController userController =Get.find<UserController>();
   @override
   void initState() {
-
-   // userController.initialNavigate();
+    // userController.initialNavigate();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white ,
+      backgroundColor: Colors.white,
       body: Center(
-        child:Image.asset("",
-          height: 110,
-          width: 258,
+        child: SvgPicture.asset(
+          "assets/officiallogo.svg",
+          height: Get.height * 0.15,
+          width: Get.width * 0.4,
+          color: Color(0xff12B5D6),
         ),
       ),
     );
   }
 }
-
-
-
